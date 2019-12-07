9c329c0e1da3608270c17be5c6a99e8310f347ed
Hao Li <haoli2@andrew.cmu.edu>
2019-12-06 20:29:45 -0500

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Hao Li on 12/6/19.
+//  Copyright © 2019 Hao Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
